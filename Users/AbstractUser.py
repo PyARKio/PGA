d7462c17071ed2bbbd09ef41d9cdc6384437b5a7
PyARKio <fedoretss@gmail.com>
2020-08-06 07:01:11 +0300

    user

---
+++ Users/AbstractUser.py
@@ -0,0 +1,24 @@
+# -- coding: utf-8 --
+from __future__ import unicode_literals
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from utilits.log_settings import log
+from utilits.url_obj import URL
+
+
+__author__ = 'PyARK'
+__version__ = "1.0.1"
+__email__ = "fedoretss@gmail.com"
+__status__ = "Production"
+
+
+class AbstractUser(ABC):
+    def __init__(self, uid, name):
+        self.__id = uid
+        self.__name = name
+        self.__email = None
+
+
+
+
+
